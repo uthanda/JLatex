b6db9781c61bbf37ae39c78d0adadff515b340ce
Aidan Sciortino <2001flyingpigs14@gmail.com>
2017-06-01 10:06:12 -0400

    Changed to accomodate options for documentclass
    Modified the LatexPreamble class to acommodate an array of options for
    the documentclass, eg. \documentclass[options]{documentclass}

--- src/LatexPreamble.java
+++ src/LatexPreamble.java
@@ -3,32 +3,45 @@ import java.util.ArrayList;
 public class LatexPreamble extends LatexContent{
 	private String dclass,title,date,author;
 	private ArrayList<LatexPackage> packages;
+	private ArrayList<String> dclassopts = null;
 	public LatexPreamble(String dclass, String title, String author){
 		this.dclass = dclass;
 		this.title = title;
 		this.author = author;
+		this.dclassopts= null;
 		this.date = null;
 		this.packages = null;
 		
 	}
-	public LatexPreamble(String dclass, String title, String date, String author){
+	public LatexPreamble(String dclass, String title, String date, String author,ArrayList<String> dclassopts){
 		this.dclass = dclass;
 		this.title = title;
 		this.date = date;
 		this.author = author;
+		this.dclassopts = dclassopts;
 		this.packages = null;
 	}
-	public LatexPreamble(String dclass, String title, String date, String author, ArrayList<LatexPackage> packages){
+	public LatexPreamble(String dclass, String title, String date, String author, ArrayList<LatexPackage> packages,ArrayList<String> dclassopts){
 		this.dclass = dclass;
 		this.title = title;
 		this.date = date;
 		this.author = author;
 		this.packages = packages;
+		this.dclassopts = dclassopts;
 	}
 	@Override
 	public String toLatexCode() {
 		String out = "";
-		out = out + "\\documentclass{" + dclass + "}\n\n";
+		out = out + "\\documentclass";
+		if(dclassopts != null){
+			out += "[";
+			for(String s : dclassopts){
+				out += s;
+				if(dclassopts.indexOf(s) != dclassopts.size() - 1) out += ",";
+			}
+			out += "]";
+		}
+		out += "{" + dclass + "}\n\n";
 		if(packages != null){
 			for(LatexPackage l : packages){
 				out += l.toLatexCode();
